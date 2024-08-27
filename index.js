b90a121a67fede8c4c0bcbcbb503575cca66e7d1
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2024-08-27 11:41:16 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear o corpo das requisições como JSON
+app.use(bodyParser.json());
+
+// Configuração das chaves e URL do endpoint do Anapro
+const ANAPRO_ENDPOINT = "https://crm.anapro.com.br/webcrm/webapi/integracao/v2/CadastrarProspect";
+const KEY = "wz2O9Z9BawY1";
+const CANAL_KEY = "7aTeATm50Tk1";
+const CAMPANHA_KEY = "T8Ds8DuFA781";
+const KEY_INTEGRADORA = "883F81F3-32BF-4A1F-BE1D-71E93E900832";
+const KEY_AGENCIA = "883F81F3-32BF-4A1F-BE1D-71E93E900832";
+
+// Rota para o webhook
+app.post('/webhook', async (req, res) => {
+    const { name, email, phone, google_ads } = req.body;
+
+    const body = {
+        Key: KEY,
+        CanalKey: CANAL_KEY,
+        CampanhaKey: CAMPANHA_KEY,
+        PoliticaPrivacidadeKey: "",
+        PessoaNome: name,
+        PessoaEmail: email,
+        KeyIntegradora: KEY_INTEGRADORA,
+        KeyAgencia: KEY_AGENCIA,
+        Midia: google_ads?.utmSource || "site-organico",
+        Peca: google_ads?.utmCampaign || null,
+        PessoaTelefones: [
+            {
+                DDD: phone.slice(0, 2),
+                Numero: phone.slice(2),
+            },
+        ],
+    };
+
+    try {
+        const response = await fetch(ANAPRO_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to submit form");
+        }
+
+        console.log("Lead enviado com sucesso:", body);
+        res.status(200).send({ message: "Lead enviado com sucesso!" });
+    } catch (error) {
+        console.error("Erro ao enviar lead:", error);
+        res.status(500).send({ error: "Erro ao enviar lead" });
+    }
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
