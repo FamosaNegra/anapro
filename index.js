56946acab4208e90a2d24e14109801b95427226b
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2024-11-05 18:44:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -93,6 +93,56 @@ app.post('/webhook', async (req, res) => {
 });
 
 
+app.post('/black', async (req, res) => {
+    const userColumnData = req.body.user_column_data || [];
+
+    // Extraímos os dados usando o column_id
+    const name = extractUserData(userColumnData, "FULL_NAME") || "Nome Desconhecido";
+    const email = extractUserData(userColumnData, "EMAIL") || "email@desconhecido.com";
+    let phone = extractUserData(userColumnData, "PHONE_NUMBER") || "";
+
+    // Extraímos o DDD e o número do telefone
+    const { DDD, Numero } = extractPhoneData(phone);
+
+    // Se o telefone for inválido (sem DDD e sem número), pode ser registrado como nulo
+    const pessoaTelefones = DDD && Numero ? [{ DDD, Numero }] : [];
+
+    const body = {
+        Key: KEY,
+        CanalKey: CANAL_KEY,
+        CampanhaKey: CAMPANHA_KEY,
+        PoliticaPrivacidadeKey: "",
+        PessoaNome: name,
+        PessoaEmail: email,
+        KeyIntegradora: KEY_INTEGRADORA,
+        KeyAgencia: KEY_AGENCIA,
+        PessoaTelefones: pessoaTelefones,
+        Data: new Date().toISOString(), // Adicionando a data e hora ao corpo do envio
+        Midia: "google-ads",
+        Peca: "Black-Friday",
+    };
+
+    try {
+        const response = await fetch(ANAPRO_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to submit form");
+        }
+
+        console.log("Lead enviado com sucesso:", body);
+        res.status(200).send({ message: "Lead enviado com sucesso!" });
+    } catch (error) {
+        console.error("Erro ao enviar lead:", error);
+        res.status(500).send({ error: "Erro ao enviar lead" });
+    }
+});
+
 // Inicia o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
